79b643e25f4b87221d43d79d53d3b07de10489d6
Alexander <stdAlexTikhonov@hotmail.com>
2023-11-06 13:54:15 +0300

    fix(TASK-2): updated data generator

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,20 @@
-import { Column, Row, Data } from './types';
+import { ColumnDef } from '@tanstack/react-table';
+import { Row, Data } from './types';
 
 const numGenerate = (num: number) => Math.floor(Math.random() * num) + 1;
 
-const makeColumn = (_: number, num: number): Column => `Col-${num}`;
+const makeBaseColumn = (_: number, num: number): ColumnDef<Row> => ({
+    id: `Col-${num}`
+});
+
+export const makeColumn = <T>(column: ColumnDef<T>, options: ColumnDef<T>): ColumnDef<T> => ({
+    ...column,
+    ...options
+});
 
 export const makeRow = (cols: number): Row => Array(cols).fill(0).reduce((a, b, i) => ({...a, [`Col-${i}`]: numGenerate(cols - i) }), {});
 
-export const makeData = (len: number, cols: number = 10): Data => ({
-    columns: Array(cols).fill(0).map(makeColumn),
+export const makeData = (len: number, cols: number = 10): Data<Row> => ({
+    columns: Array(cols).fill(0).map(makeBaseColumn),
     data: Array(len).fill(0).map(_ => makeRow(cols))
 })
